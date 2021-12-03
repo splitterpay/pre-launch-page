2e5b19b751781da2fe4986f83544706330307421
Stephen Appiah <appstephen8@gmail.com>
2021-12-03 21:32:49 +0000

    Updates

---
+++ precache-manifest.9c85dac476f96ef7d5b97da40c1aaf43.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "128fedd380924d225ee902e505494b74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3850ca9dab7929d1ec52",
+    "url": "/static/css/main.cddc8372.chunk.css"
+  },
+  {
+    "revision": "2ff37216124e8362c019",
+    "url": "/static/js/2.37ec18ce.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.37ec18ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3850ca9dab7929d1ec52",
+    "url": "/static/js/main.d1962909.chunk.js"
+  },
+  {
+    "revision": "dcbd4573a2bef40dd88d",
+    "url": "/static/js/runtime-main.9ae8de6b.js"
+  },
+  {
+    "revision": "633d13b4d49eff157414c8c795836c8e",
+    "url": "/static/media/img-1.633d13b4.jpg"
+  },
+  {
+    "revision": "df636ba9ef12edb0a19dee0e681b09eb",
+    "url": "/static/media/img-2.df636ba9.jpg"
+  },
+  {
+    "revision": "b572f2503072280c3bcc90955c0c0dd5",
+    "url": "/static/media/img-3.b572f250.jpg"
+  },
+  {
+    "revision": "a4af5cac92d91e4fd7d2558a758bf2a9",
+    "url": "/static/media/img-4.a4af5cac.jpg"
+  }
+]);
